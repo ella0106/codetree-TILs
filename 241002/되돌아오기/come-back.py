54097294a3abf548ca593f83cdd9211d8a58668d
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-02 14:55:48 +0900

    Title: 되돌아오기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/되돌아오기/come-back.py
@@ -0,0 +1,25 @@
+num = int(input())
+mapper = {
+    'N' : [0, 1],
+    'S' : [0, -1],
+    'E' : [1, 0],
+    'W' : [-1, 0]
+}
+
+x, y = 0, 0
+cnt = 0
+answer = 0
+
+for i in range(num):
+    a = input().split()
+    for j in range(int(a[1])):
+        cnt += 1
+        x, y = x + mapper[a[0]][0], y + mapper[a[0]][1]
+        if x == 0 and y == 0:
+            answer = cnt
+            break
+
+if answer == 0:
+    print(-1)
+else:
+    print(answer)
