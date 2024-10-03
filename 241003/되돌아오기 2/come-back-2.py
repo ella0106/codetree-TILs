9b2f3cffb2fbf276b47a0e8fe60ceb7279776f88
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-03 23:19:34 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241003/되돌아오기 2/come-back-2.py
@@ -0,0 +1,22 @@
+road = input()
+mapper = {
+    'F' : 0,
+    'R' : 1,
+    'L' : -1,
+}
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+x, y, dir_num = 0, 0, 0
+
+for i in range(len(road)):
+    change = mapper[road[i]]
+    if change == 0:
+        x, y = x + dx[dir_num], y + dy[dir_num]
+    else:
+        dir_num += mapper[road[i]]
+    if x == 0 and y == 0:
+        print(i+1)
+        break
+
+if x != 0 or y != 0:
+    print(-1)
