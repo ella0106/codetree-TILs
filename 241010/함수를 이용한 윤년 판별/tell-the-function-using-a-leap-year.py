3459647a304af52d80307aca4b782756bf9c0552
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-10 20:57:49 +0900

    Title: 함수를 이용한 윤년 판별, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 241010/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,8 @@
 def is_yoon(n):
     if n % 100 == 0 and n % 400 != 0:
-        return False
+        return 'false'
     if n % 4 == 0:
-        return True
-    return False
+        return 'true'
+    return 'false'
 
 print(is_yoon(int(input())))
