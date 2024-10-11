d7a1b8688e2491723914e2f1ccceefe727554feb
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-11 10:25:22 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 241011/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,5 +1,5 @@
 def is_sosu(n):
-    for i in range(2, n+1):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
@@ -8,7 +8,7 @@ def is_even(n):
     n = str(n)
     total = 0
     for i in range(len(n)):
-        total += n[i]
+        total += int(n[i])
     if total % 2 == 0:
         return True
     return False
@@ -16,6 +16,7 @@ def is_even(n):
 a, b = tuple(map(int, input().split()))
 cnt = 0
 for i in range(a, b+1):
+    #print(i, is_sosu(i), is_even(i))
     if is_sosu(i) and is_even(i):
         cnt += 1
 
