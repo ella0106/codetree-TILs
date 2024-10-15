75d2513016475a566bb316f55b32c3288a2b6c55
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-15 11:11:27 +0900

    Title: 두 정수에 대한 연산값, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 241015/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,9 +1,11 @@
 a, b = tuple(map(int, input().split()))
 def answer(a, b):
-    m = max(a, b)
-    m += 25
-    mi = min(a, b)
-    mi *= 2
-    print('{} {}'.format(mi, m))
+    if a > b:
+        a += 25
+        b *= 2
+    else:
+        a *= 2
+        b += 25
+    print('{} {}'.format(a, b))
 
 answer(a, b)
