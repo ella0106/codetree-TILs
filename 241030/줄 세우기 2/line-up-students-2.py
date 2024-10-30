9d61d4578d90e4bfc5140708603a94f5652cf5d1
박지연 <42113966+ella0106@users.noreply.github.com>
2024-10-30 23:58:36 +0900

    Title: 줄 세우기 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/줄 세우기 2/line-up-students-2.py
+++ 241030/줄 세우기 2/line-up-students-2.py
@@ -10,6 +10,6 @@ for i in range(N):
     h, w = tuple(map(int, input().split()))
     st_list.append(Student(h, w, i+1))
 
-st_list.sort(key=lambda x : (x.h, x.w))
+st_list.sort(key=lambda x : (x.h, -x.w))
 for st in st_list:
     print(st.h, st.w, st.n)
