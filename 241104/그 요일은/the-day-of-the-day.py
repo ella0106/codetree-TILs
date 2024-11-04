34e4867dbf7c84112af2348b096fdc1887e1f698
박지연 <42113966+ella0106@users.noreply.github.com>
2024-11-04 17:39:33 +0900

    Title: 그 요일은, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/그 요일은/the-day-of-the-day.py
+++ 241104/그 요일은/the-day-of-the-day.py
@@ -1,17 +1,13 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 d1 += 5
-cnt = 1
-while True:
-    if m1 >= m2 and d1 >= d2:
-        break
+days = 0
+for m in range(m1, m2):
+    days += months[m]
+    #print(m, days)
 
-    if d1 > months[m1]:
-        d1 -= months[m1]
-        m1 += 1
+days += d2
+days -= d1
+#print(days)
 
-    d1 += 7
-    cnt += 1
-    # print(m1, d1, m2, d2)
-
-print(cnt)
+print(days // 7 + 1)
