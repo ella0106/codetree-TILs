1ba18d7db2c30dbb4e4efde994d97af1a914e58c
박지연 <42113966+ella0106@users.noreply.github.com>
2024-11-04 17:51:49 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/DateTime to DateTime/datetime-to-datetime.py
+++ 241104/DateTime to DateTime/datetime-to-datetime.py
@@ -1,19 +1,22 @@
 a, b, c = tuple(map(int, input().split()))
 day, hour, minute = 11, 11, 11
 elapsed = 0
-while True:
-    if day == a and hour == b and minute == c:
-        break
+if day > a or hour > b or minute > c:
+    print(-1)
+else:
+    while True:
+        if day == a and hour == b and minute == c:
+            break
 
-    minute += 1
-    elapsed += 1
+        minute += 1
+        elapsed += 1
 
-    if minute == 60:
-        minute = 0
-        hour += 1
-    
-    if hour == 24:
-        hour = 0
-        day += 1
+        if minute == 60:
+            minute = 0
+            hour += 1
+        
+        if hour == 24:
+            hour = 0
+            day += 1
 
-print(elapsed)
+    print(elapsed)
